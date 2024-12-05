2980ff93e48f5faf2bdadb7e7159f73f6be621f8
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-05 14:48:08 +0800

    Update main5.c

--- EX3/main5.c
+++ EX3/main5.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=20 ;
+    if(i >=240 ){
+        printf("240元");
+    }
+    else if(i<=30 ){
+        printf("免費");
+    }
+    else{
+        if(i %30){
+            int cost =((i/30)+1)*30 ;
+            printf("%d元", cost);
+        }
+        else{
+             printf("%d元", i);
+        }
+    }
+    return 0;
+}
 
