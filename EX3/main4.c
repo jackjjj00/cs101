5e6dfa8a45b8d8cbe2906ca73da712a050ac6040
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-05 14:34:07 +0800

    Update main4.c

--- EX3/main4.c
+++ EX3/main4.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=1900 ;
+    if((i%4==0 && i%100!=0)||i%400==0){
+        printf("閏年");
+    }
+    else
+        printf("不是閏年");
+   
+
+    return 0;
+}
 
