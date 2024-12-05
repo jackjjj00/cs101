d26a317362bce4971ec677dfeeb2031fda0d51e9
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-05 15:06:11 +0800

    Update main6.c

--- EX3/main6.c
+++ EX3/main6.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=3000 ;
+    if(i <=1500 ){
+        printf("70元");
+    }
+    else{
+        int k= i-1500 ;
+        
+        if(k%100){
+            int cost =((k/100)+1)*10 +70 ;
+            printf("%d元", cost);
+        }
+        else{
+            k=70+k/10 ;
+            printf("%d元", k);
+        }
+    }
+    return 0;
+}
 
