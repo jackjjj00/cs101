37548b7996ebfb29d4411ae13b7d628ed694db45
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-05 15:16:20 +0800

    Update main8.c

--- EX3/main8.c
+++ EX3/main8.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x=-1 ;
+    int y=2 ;
+    int z=3 ;
+    int sum=x*100+y*10+z;
+    if(x<0){
+        sum=x*-100+y*10+z;
+        printf("%d", sum*-1);
+    }
+    else
+        printf("%d", sum);
+    
+    return 0;
+}
 
