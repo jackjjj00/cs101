b36b744fa0eda6d586a03d28e8a744fd7d62429e
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-08 03:15:54 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main()
+{   
+    int x=9;
+    for(int y=1;y<=x*x;y++){
+        int ver =(y-1)/x+1; //行數
+        int hor =(y-1)%x+1;//列數
+        printf("%d*%d=%d\t",ver,hor,ver*hor);
+        //列數9的時候換行
+        if(hor==x){
+             printf("\n"); 
+        }
+    }
+}
 
