73d15bb5253b8162fbcd40a914d9332ced678b5a
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-08 01:56:05 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int x=1;
+    int sign =1;
+    double PI =4.0f ;
+
+     for(int x=3;;x+=2){
+        sign*=-1;
+        PI+=sign*(4.0/x);
+        if(fabs(PI-3.14159)<=0.000005){
+            printf("PI= %.5f\n",PI);
+            printf("x= %d",x-2);
+            break;
+         }
+     }
+
+    return 0;
+}
 
