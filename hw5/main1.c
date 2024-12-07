234c9918b3f33f64c290e5408352ca15fb33407e
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-08 00:17:09 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=7,j;
+    
+    for(int i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+        if(j+i>=n+1){
+            printf("%d ", i);
+        }
+        else{
+           printf(" "); 
+        }
+        
+        }
+         printf("\n");
+    
+    }
+
+    return 0;
+}
 
