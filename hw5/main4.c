e8cd73f19898e36c4b2c8943d8eefc96539a5ed6
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-09 00:06:34 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){   
+    int i = 12345,h,k;
+    if(i<1000){
+        h=i%10; //取個位數
+        i=h*1000+i-h;
+        printf("%d",i);
+    }
+    else{
+        h=i%10;//取個位數
+        //取千位數
+        k=i/1000;
+        while(k>10){
+            k%=10;
+        }
+        
+        i=i+(999*abs(h-k));
+        printf("%d",i);
+    }
 
+    return 0;
+}
