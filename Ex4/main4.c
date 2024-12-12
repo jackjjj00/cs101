fb90687d5a5f38954be2c4eb417b90c76eecf17d
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-12 15:14:02 +0800

    Update main4.c

--- Ex4/main4.c
+++ Ex4/main4.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int i=12345,d=0 ;
+    d=i%10;
+    if(i<1000){
+        i=d*1000+i-d;
+        printf("%d",i);
+    }
+    else{
+        int k=i/1000;
+        if(k%10){
+            k%=10;
+            i=i+abs(d-k)*999;
+        }
+        printf("%d",i);
+    }
+    return 0;
+}
