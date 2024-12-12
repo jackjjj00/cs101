f1c97565fb50059f166ec6aaf07408f11650123f
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-12 14:55:35 +0800

    Update main1.c

--- Ex4/main1.c
+++ Ex4/main1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n=7;
+    for(int i=1;i<=n;i++){
+        for(int j=1; j<=n;j++){
+            if(i+j>=n+1){
+                printf(" %d", i);
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
 
+    return 0;
+}
