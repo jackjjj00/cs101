6cae45cc3706735f3fd202477fdd75990291aaf0
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2024-12-12 15:27:13 +0800

    Update main2.c

--- Ex4/main2.c
+++ Ex4/main2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int x=1;
+    long ipi =0;
+    int sign =1;
+    double PI =4.0f ;
 
+     for(int x=3;;x+=2){
+        sign*=-1;
+        PI+=sign*(4.0/x);
+        ipi=PI*100000;
+        if(ipi==314159){
+            printf("PI= %.5f\n",PI);
+            printf("x= %d",x);
+            break;
+         }
+     }
+
+    return 0;
+}
