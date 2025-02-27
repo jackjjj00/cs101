61e0fab9222cab3c5bbd10a6bb5811fce57a5799
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2025-02-27 14:13:24 +0800

    Update main4.c

--- qz1/main4.c
+++ qz1/main4.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main()
+{   
+    int x=9;
+    for(int y=1;y<=x*x;y++){
+        int ver =(y-1)/x+1; //行
+        int hor =(y-1)%x+1;//列
+        printf("%d*%d=%d\t",ver,hor,ver*hor);
+        //列數9的時候換行
+        if(hor==x){
+             printf("\n"); 
+        }
+    }
+    return 0 ;
+}
