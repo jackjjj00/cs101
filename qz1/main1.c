df0903ba18e83f7d8719553794532f31244dde7a
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2025-02-27 13:54:20 +0800

    Update main1.c

--- qz1/main1.c
+++ qz1/main1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main()
+{   
+    int i=1540;
+    int k=i-1500;
+    if(k<=0){
+        printf("70元\n");
+    }
+    else if (k<=100){
+         printf("80元\n");
+    }
+    else{
+        int count =k/100 ;
+        if(k%100){
+          int sum = (count+1)*10+70;
+          printf("%d元\n", sum);
+        } 
+        else{
+            int sum = (count)*10+70;
+            printf("%d元\n", sum);
+        }
+    }
+    return 0;
+}
