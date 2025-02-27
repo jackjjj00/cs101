a7eb2240e87c6ac1432ac3869fd134d2c79348e8
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2025-02-27 14:41:00 +0800

    Update mian5.c

--- qz1/mian5.c
+++ qz1/mian5.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main()
+{   
+    int n=32;
+    int j=0;
+    while(n){
+        if(n&1){
+            n/=2;
+            j++;  
+        }
+        n>>=1;
+        }
+        printf("%d\n",j);
+   
+    return 0 ;
+}
