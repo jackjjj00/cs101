d3ec301c44abb2e86c9f2c2aed3a7f7e391ef72e
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2025-03-06 15:25:46 +0800

    Update main1.c

--- test/main1.c
+++ test/main1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main()
+{   
+    int count =1;
+    char a[]= "AABBCCCddd" ; 
+    for(int i=0;i<sizeof(a);i++){
+
+        if(a[i]!=a[i+1]){
+            count =1;
+        }
+        else{
+            count ++;
+            printf("%c%d",a[i] , count );
+        }
+
+
+    }
+
+    return 0;
+}
 
