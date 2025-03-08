6cf6841c4f3b95b593a98070b61ae519aa70d66e
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2025-03-09 04:06:08 +0800

    Update main5.c

--- test/main5.c
+++ test/main5.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+void print_sp (int n){
+    for(int i=1;i<=n;i++){
+        printf(" ");
+    }
+
+}
+void print_num (int num){
+    for (int k=1 ;k<=num;k++){
+        printf(" %d", num);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        print_sp(n-i);
+        print_num(i);
+    }
+
+
+    return 0;
+}
