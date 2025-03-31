f8f4299a80ba4ae2d344902d63bc4006b2f5999a
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2025-04-01 01:04:03 +0800

    Update HW3.c

--- test/HW3.c
+++ test/HW3.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+void replace_all(char* str, char oldChar, char newChar){
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]==oldChar){
+            str[i]=newChar;
+        }
+    }
+    
+}
+
+int main(){
+    char str[100], oldChar = 'a', newChar = 'A';
+    scanf("%s", str);
+
+    replace_all(str, oldChar, newChar);
+    printf("%s", str);
+
+    return 0;
+}
+
 
