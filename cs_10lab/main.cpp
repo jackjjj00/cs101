7466abaf6d3992d3122120438187ef70b6d71c71
jackjjj00 <108651250+jackjjj00@users.noreply.github.com>
2025-05-22 15:49:26 +0800

    Update main.cpp

--- cs_10lab/main.cpp
+++ cs_10lab/main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+class Animal {
+    public:
+    virtual void func1()=0;
+};
+class Dog : public Animal{
+    public:
+    virtual void func1(){
+        cout<<"I am dog and I eat meat"<<endl;
+    }
+};
+class Cat: public Animal {
+    public:
+    virtual void func1(){
+        cout<<"I am cat and I eat fish"<<endl;
+    }
+};
 
+int main()
+{
+    Dog D;
+    D.func1();
+    Cat C;
+    C.func1();
+    return 0;
+}
